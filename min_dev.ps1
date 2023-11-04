0cc4aa4dd5fdeb227df490e531f5e8f7e745460c
Nicolas Tagliani <nicolas.tagliani@gmail.com>
2023-11-04 19:00:30 +0100

    Added default installation path for python

--- min_dev.ps1
+++ min_dev.ps1
@@ -4,7 +4,7 @@ choco install -y git-lfs
 choco install -y cmake --installargs 'ADD_CMAKE_TO_PATH=User ALLUSERS=0 DESKTOP_SHORTCUT_REQUESTED=0'
 choco install -y ninja
 choco install -y vscode
-choco install -y python
+choco install -y python --params "/InstallDir:C:\python3"
 choco install -y msys2 --params "/NoUpdate /InstallDir:C:\msys64"
 choco install -y microsoft-windows-terminal
 
